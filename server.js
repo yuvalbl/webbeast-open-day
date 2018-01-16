be4411d0608907185505a1981a9011405d264e6f
Yuval <yuvalbl@users.noreply.github.com>
2018-01-16 20:49:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 
 server.listen(3001, function(){
     console.log('Client is available at http://localhost:3001');
