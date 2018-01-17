d69797b3f25a58f937e8f93d3532158f9f535577
yuvalbl <yuval.bar.levi@gmail.com>
2018-01-17 11:07:43 +0200

    basic static server

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 
+// serve client files
+app.use('/client/public', express.static(path.join(__dirname + '/client/public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'))
+});
+
 server.listen(3001, function(){
     console.log('Client is available at http://localhost:3001');
 });
